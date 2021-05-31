cd06b59a4b2244e8c4546f85677254d334dfabe6
ashwin nirmul da21-01-6311 <ashwin.metropcs@gmail.com>
2021-05-31 17:57:22 -0400

    Latest changes.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,53 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import './App.css';
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value
+  }
+}
+
 function App() {
+  const [formData, setFormData] = useReducer(formReducer, {});
   const [submitting, setSubmitting] = useState(false);
+
   const handleSubmit = event => {
     event.preventDefault();
     setSubmitting(true);
 
-    setTimeout(() => {
-      setSubmitting(false);
-    }, 3000)
+    // setTimeout(() => {
+    //   setSubmitting(false);
+    // }, 3000)
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
   }
 
   return (
     <div className="wrapper">
-      <h1>How About Them Apples</h1>
+      <h1>Get your weather here!</h1>
       {submitting &&
-        <div>Submtting Form...</div>
+        <div> The weather for:
+          <ul>
+            {Object.entries(formData).map
+              (([name, value]) =>
+              (<li key={name}><strong>{name}</strong>:{value.toString()}</li>
+              )
+              )
+            }
+          </ul>
+        </div>
       }
       <form onSubmit={handleSubmit}>
         <fieldset>
           <label>
-            <p>Name</p>
-            <input name="name" />
+            <p>City</p>
+            <input name="name" onChange={handleChange} />
           </label>
         </fieldset>
         <button type="submit">Submit</button>
